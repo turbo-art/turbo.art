ba267ef5da62a220ecd615e81fca8cefbd7edaa1
John Slevinsky <john.slev@gmail.com>
2016-11-27 19:58:24 -0800

    yeah

--- turbo/src/App.js
+++ turbo/src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 import Dropzone from 'react-dropzone'
 
 class App extends Component {
+
+  onDrop() {
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>turbo.art</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          art with extra horsepower
         </p>
         <Dropzone onDrop={this.onDrop}>
           <div> drop yo files here </div>
