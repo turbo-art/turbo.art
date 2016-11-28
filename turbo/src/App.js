8eb64ee3cb6d15e4c9325ba8fe24f3dc3e4f08b8
John Slevinsky <john.slev@gmail.com>
2016-11-27 19:54:24 -0800

    adding react dropzone

--- turbo/src/App.js
+++ turbo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Dropzone onDrop={this.onDrop}>
+          <div> drop yo files here </div>
+        </Dropzone>
       </div>
     );
   }
